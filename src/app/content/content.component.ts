f1cdaf1303d9cff38399fd39e6cfcb97d473be5b
niluiz <nicoleduarteluna@hotmail.com>
2019-10-02 22:26:58 -0300

    Introduzindo animação TYPED

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import Typed from 'typed.js';
+
 @Component({
   selector: 'has-content',
   templateUrl: './content.component.html',
@@ -10,6 +12,20 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['ETEC ', 'Seja bem-vindo', 'Inovavor, digital e seguro', 'Uma nova chance'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+
+    const typed = new Typed('.typing-element', options);
   }
 
 }
